40ad637d8e42f5a7e194dbda864b38e68c58d1a2
kyung-Park <ruddnjs5816@naver.com>
2022-11-25 10:57:52 +0900

    main, account upload

--- sparta_java/src/main/java/Account.java
+++ sparta_java/src/main/java/Account.java
@@ -5,12 +5,31 @@ public class Account {
     public int money;
     public String bankName;
 
-    public Account(String accountNumber, String name, String pwd, int money, String bankName)
-    {
+    public Account(String accountNumber, String name, String pwd, int money, String bankName) {
         this.accountNumber = accountNumber;
         this.name = name;
         this.pwd = pwd;
         this.money = money;
         this.bankName = bankName;
     }
-}
+        public String getAccountNumber () {
+            return accountNumber;
+        }
+
+        public String getName () {
+            return name;
+        }
+
+        public String getPwd () {
+            return pwd;
+        }
+
+        public int getMoney () {
+            return money;
+        }
+
+        public String getBankName () {
+            return bankName;
+        }
+    }
+
